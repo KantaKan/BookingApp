9393c7e3daa6611bd46b31abb4c004a019ee7251
fia <fia@192.168.1.46>
2024-03-05 02:31:44 +0700

    write some check

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,35 @@
-import express from "express"
+import express from "express";
+import {
+  getUser,
+  deleteUser,
+  createUser,
+  updateUser,
+  getUsers,
+} from "../controller/usercontroller.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("hello youre login to check auth and acc is authenticated");
+// });
 
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("youre logged in and allow to delete your account ");
+// });
 
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("you're admin kub");
+// });
 
-export default router
+router.post("/", createUser);
+//Update
+router.put("/:id", updateUser);
+//Delete
+router.put("/:id", deleteUser);
+//Get
+router.get("/:id", getUser);
+//Get All
+router.get("/", getUsers);
+
+export default router;
--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import authRoute from "./api/routes/auth.js"
-import hotelsRoute from "./api/routes/hotels.js"
-import roomRoute from "./api/routes/room.js"
-import usersRoute from "./api/routes/users.js"
-const app = express()
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./api/routes/auth.js";
+import hotelsRoute from "./api/routes/hotels.js";
+import roomRoute from "./api/routes/room.js";
+import usersRoute from "./api/routes/users.js";
+import cookieParser from "cookie-parser";
 
+const app = express();
+dotenv.config();
 
 const connect = async () => {
-
-try {
+  try {
     await mongoose.connect(process.env.MONGO);
-    console.log("connected to mongo")
-} catch (err){
-    throw error; 
-}
+    console.log("connected to mongo");
+  } catch (err) {
+    throw error;
+  }
 };
 
-mongoose.connection.on("disconnected",()=> {
-    console.log("mongodb disconnected ")
-})
-mongoose.connection.on("connected",()=> {
-    console.log("mongodb connected ")
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected ");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connected ");
+});
 
 ///middleware
-
-app.use(express.json())
-
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
+app.use("/api/user", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/room", roomRoute);
 
-app.use((err,req,res,next) => {
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || "something went wrong"
-    return res.status(errorStatus).json({
-        success:false,
-        status:errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-    })
-})
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
-
-
-app.listen(8800, ()  => {
-    connect()
-    console.log('connected to backend alreadys')
-})
+app.listen(8800, () => {
+  connect();
+  console.log("connected to backend already");
+});
